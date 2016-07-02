48861d39f9d6e80fd4a967ff5330d75659de706b
Amit <amitgurav04@gmail.com>
2016-07-02 17:13:07 +0530

    implement repay in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :payments
 	has_many :bills, :through => :payments
+
+
+	def total_paid
+		self.payments.sum("amount")
+	end
+
+	def total_repay
+		average = bills.sum("amount")/User.count
+		average - total_paid
+	end
 end
